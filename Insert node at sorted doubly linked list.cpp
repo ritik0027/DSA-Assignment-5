f6e9d133620d02100ee393af76ea0e66ebde95d5
Ritik Kumar <114002245+ritik0027@users.noreply.github.com>
2022-10-29 22:08:10 +0530

    Update Insert node at sorted doubly linked list.cpp

--- Insert node at sorted doubly linked list.cpp
+++ Insert node at sorted doubly linked list.cpp
@@ -1,12 +1,29 @@
 DoublyLinkedListNode* sortedInsert(DoublyLinkedListNode* llist, int data){
-    DoublyLinkedListNode* temp=new DoublyLinkedListNode(data);
+    DoublyLinkedListNode* temp= new DoublyLinkedListNode(data);
+    temp->next=NULL;
+    temp->prev=NULL;
     if(llist==NULL){
         return temp;
     }
-    if(temp->data<llist->data){
+    else if(temp->data < llist->data){
         temp->next=llist;
         llist->prev=temp;
         llist=temp;
+     }
+    else {
+         DoublyLinkedListNode* p=llist;
+         while(temp->data > p->data){
+             if(p->next==NULL){
+                 p->next=temp;
+                 temp->prev=p;
+                 return llist;
+             }
+         p=p->next;
+         }
+         p->prev->next=temp;
+         temp->prev=p->prev;
+         temp->next=p;
+         p->prev=temp;
     }
     return llist;
 }
